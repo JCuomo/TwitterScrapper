0d22ccea1e525f4638fd3f4b57222a2236251d23
joaquin <joaquin@joaquin-Lenovo>
2019-03-15 10:39:48 -0600

    working? example of hashtags count based on https://www.tutorialspoint.com/apache_storm/apache_storm_in_twitter.htm

---
+++ src/twitter/HashtagCounterBolt.java
@@ -0,0 +1,65 @@
+package twitter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+@SuppressWarnings("serial")
+public class HashtagCounterBolt implements IRichBolt {
+   Map<String, Integer> counterMap;
+   private OutputCollector collector;
+
+   @Override
+   public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+      this.counterMap = new HashMap<String, Integer>();
+      this.collector = collector;
+   }
+
+   @Override
+   public void execute(Tuple tuple) {
+      String key = tuple.getString(0);
+
+      if(!counterMap.containsKey(key)){
+         counterMap.put(key, 1);
+      }else{
+         Integer c = counterMap.get(key) + 1;
+         counterMap.put(key, c);
+      }
+		
+      collector.ack(tuple);
+   }
+
+   @Override
+   public void cleanup() {
+      for(Map.Entry<String, Integer> entry:counterMap.entrySet()){
+         System.out.println("Result: " + entry.getKey()+" : " + entry.getValue());
+      }
+   }
+
+   @Override
+   public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("hashtag"));
+   }
+	
+   @Override
+   public Map<String, Object> getComponentConfiguration() {
+      return null;
+   }
+	
+}
---
+++ src/twitter/TwitterHashtagStorm.java
@@ -0,0 +1,39 @@
+package twitter;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+
+public class TwitterHashtagStorm {
+	public static void main(String[] args) throws Exception {
+
+		String accessToken = "1105927449991274496-6CjiPkops1nOQDyTXdTwss7GeQDQS3";
+		String accessTokenSecret = "i4nUp8uhx2wIfVzVUQ3us83Hx9wGTn24CVFMxdDe0633b";
+		String consumerKey = "WTBu0qpuiiHNdvzgjl1dbAkqS";
+		String consumerSecret = "H8WCvRD28g5RO5FwExRwqpeI7IPVkKoxpNRF90ObyYgOOvlnvh";
+
+		//String[] arguments = args.clone();
+		//String[] keyWords = Arrays.copyOfRange(arguments, 4, arguments.length);
+		//String[] keyWords = {"zidane", "maduro"};
+		String[] keyWords = {};
+
+		Config config = new Config();
+		//config.setDebug(true);
+		config.put(Config.TOPOLOGY_DEBUG, false);
+
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("twitter-spout",
+				new TwitterStormSpout(consumerKey, consumerSecret, accessToken, accessTokenSecret, keyWords));
+
+		builder.setBolt("twitter-hashtag-reader-bolt", new HashtagReaderBolt()).shuffleGrouping("twitter-spout");
+
+		builder.setBolt("twitter-hashtag-counter-bolt", new HashtagCounterBolt())
+				.fieldsGrouping("twitter-hashtag-reader-bolt", new Fields("hashtag"));
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("TwitterHashtagStorm", config, builder.createTopology());
+		Thread.sleep(10000);
+		cluster.shutdown();
+	}
+}
